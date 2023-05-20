9c009d6218dfd6c89300b3d537932f2fdc800473
alexandrafribeiro <fariasalexandraribeiro@gmail.com>
2023-05-20 14:23:13 -0300

    Testes unitários da função calcular valor por hora

---
+++ src/entregasDesafio/alexandra-ribeiro/valorHora.test.js
@@ -0,0 +1,20 @@
+const { calcularValorPorHora } = require("../../dominio/calculadora/Hora/valorHora")
+
+
+
+describe("Calcular valor por hora", () => {
+
+    const salarioMinimo = 1300
+    const valorPorHoraCalculado = calcularValorPorHora(salarioMinimo);
+    const valorPorHoraEsperado = 8
+
+    test("Salário mínimo de 1300 reais", () => {
+        
+        expect(valorPorHoraCalculado).toBe(valorPorHoraEsperado)
+        expect(valorPorHoraCalculado).toBeGreaterThan(7)
+    })
+
+    test("", () => {
+
+    })
+})
