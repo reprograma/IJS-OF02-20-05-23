30547101d4e5791eb77e0bb9a425a2720da32521
patricia <pcbandeira.m@gmail.com>
2023-05-20 14:22:48 -0300

    Testes unitários da função calcularValorPorHora

---
+++ src/entregasDesafio/patricia-melo/valorHora.test.js
@@ -0,0 +1,18 @@
+const { calcularValorPorHora } = require("../../dominio/calculadora/Hora/valorHora");
+
+describe("Calcular Valor Por Hora", () => {
+  const salarioMinimo = 1300
+  const valorPorHoraCalculado = calcularValorPorHora(salarioMinimo)
+
+  test("Valor por hora para um salário mínimo de 1300 reais deve ser 8 reais/hora", () => {
+    const valorPorHoraEsperado = 8
+
+    expect(valorPorHoraCalculado).toBe(valorPorHoraEsperado)
+  })
+  
+  test("Valor por hora do salário mínimo seja maior que zero", () => {
+    const zero = 0
+    expect(valorPorHoraCalculado).toBeGreaterThan(zero)
+
+  })
+})
