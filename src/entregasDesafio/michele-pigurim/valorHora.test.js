effd3a58a36960e270b95ec94ac5938c09f2d5ea
MiPigurim <michelepigurim26@gmail.com>
2023-05-20 14:36:22 -0300

    Testes unitários da função calcular ValorPorHora

---
+++ src/entregasDesafio/michele-pigurim/valorHora.test.js
@@ -0,0 +1,15 @@
+const {
+  calcularValorPorHora,
+} = require("../../dominio/calculadora/Hora/valorHora");
+
+describe("Calcular volar por hora", () => {
+  test("Salário mínimo de 1300 reais", () => {
+    const valorPorHoraReal = calcularValorPorHora(1300);
+    const valorPorHoraEsperado = 8;
+    const zero = 0;
+    expect(valorPorHoraReal).toBe(valorPorHoraEsperado);
+    expect(valorPorHoraReal).toBeGreaterThan(zero);
+  });
+
+  //   test("", () => {});
+});
