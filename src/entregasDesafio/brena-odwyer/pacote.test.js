ab25e508da520699960cb1aefc0192d5d5bab107
brena-odwyer <odwyer.brena@gmail.com>
2023-05-20 15:02:06 -0400

    adicionando testes das condições de contorno do pacote e fix da função calcularPacote que estava faltand um =

--- src/entregasDesafio/brena-odwyer/pacote.test.js
+++ src/entregasDesafio/brena-odwyer/pacote.test.js
@@ -1,11 +1,33 @@
-const { calcularPacote } = require('../../dominio/calculadora/Projeto/pacote')
+const { calcularPacote } = require('./pacote')
 
 describe('Calcular pacote', () => {
+    test('Um projeto de 40 horas deve retornar "pacote_basico"', () => {
+        const horasDeProjeto = 40
+
+        expect(calcularPacote(horasDeProjeto)).toBe("pacote_basico") // adicionar um teste para as condições (um número menor que 50; 50; um número entre 50 e 100; 100; um número entre 100 e 200 já tem; 200; um número maior que 200)
+
+    })
+
+    test('Um projeto de 50 horas deve retornar "pacote_basico"', () => {
+        const horasDeProjeto = 50
+
+        expect(calcularPacote(horasDeProjeto)).toBe("pacote_basico") 
+    })
+
+    test('Um projeto de 100 horas deve retornar "pacote_basico"', () => {
+        const horasDeProjeto = 100
+
+        expect(calcularPacote(horasDeProjeto)).toBe("pacote_intermediario") 
+    })
+
+
     test('Um projeto de 104 horas deve retornar "pacote_premium"', () => {
         const horasDeProjeto = 104
 
-        expect(calcularPacote(horasDeProjeto)).toBe("pacote_premium") // adicionar um teste para as condições (um número menor que 50; 50; um número entre 50 e 100; 100; um número entre 100 e 200 já tem; 200; um número maior que 200)
+        expect(calcularPacote(horasDeProjeto)).toBe("pacote_premium") 
 
     })
+
+    
 
 })
---
+++ src/entregasDesafio/brena-odwyer/valorProjeto.test.js
@@ -0,0 +1,2 @@
+// testar calcularValorTotalProjeto
+// importa a função, coloca o describe, com as consts que precisam, ver as condições de contorno (quando é básico, intermediário e premium), precisa ter esses 3 testes
