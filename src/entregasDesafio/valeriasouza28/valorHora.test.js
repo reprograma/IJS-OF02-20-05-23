bd654fa495e645aaceda1b6024b4e1fd7a66d6fd
valeriasouza28 <valeriasouza28@gmail.com>
2023-05-20 14:22:50 -0300

    Testes unitários

---
+++ src/entregasDesafio/valeriasouza28/valorHora.test.js
@@ -0,0 +1,10 @@
+const {
+    calcularValorPorHora
+} = require("../../dominio/calculadora/Hora/valorHora");
+
+describe("Calcular Valor hora", () => {
+    test("Salario mínimo de 1300 reais", () => {
+        expect(calcularValorPorHora(1300)).toBe(8);
+    });
+    test("", () => {});
+});
