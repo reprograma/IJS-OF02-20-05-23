cfd4257f3347d30a14c85a56a17951b06bf9f45c
danibotelho <danibl26@gmail.com>
2023-05-20 17:20:41 -0300

    add test pacote intemediario

--- src/entregasDesafio/daniela-botelho/ValorProjeto/valorProjeto.test.js
+++ src/entregasDesafio/daniela-botelho/ValorProjeto/valorProjeto.test.js
@@ -3,18 +3,14 @@ const { calcularValorTotalProjeto } = require("./valorProjeto");
 
 
 describe("Valor do Projeto", () => {
-    const listaDeFuncionalidades = [ "setup", "ssr", "construcao_1_pagina"]
-    
     const listaTodasFuncionalidades = ["setup","formulario","responsividade",
-            "otimizacao_seo", "construcao_1_pagina","integracao_mailchimp",
-            "ssr","integracao_api_propria"] 
+            "otimizacao_seo", "construcao_1_pagina","integracao_mailchimp", "ssr","integracao_api_propria"] 
     
-    const listaVazia = [ ]        
-
+    const listaIntermediaria = ["setup","formulario","responsividade", "otimizacao_seo", "construcao_1_pagina"]         
     
-    test("Valor do projeto com 3 funcionalidades de 8 horas cada e valor da hora 10,00, é 264,00" , () => {
-        expect(calcularValorTotalProjeto(listaDeFuncionalidades,10)).toBe(264);
-    })
+    const listaBasica = [ "setup", "ssr", "construcao_1_pagina"]
+    
+    const listaVazia = [ ]         
 
     test("Valor do projeto com todas as funcionalidades e valor da hora 8,00 é maior que 900", () => {
         expect(calcularValorTotalProjeto(listaTodasFuncionalidades,8)).toBeGreaterThan(900);
@@ -24,9 +20,16 @@ describe("Valor do Projeto", () => {
         expect(calcularValorTotalProjeto(listaTodasFuncionalidades,calcularValorPorHora(2200))).toBe(1555);
     })
 
+    test("Valor do projeto com 3 funcionalidades de 8 horas cada e valor da hora 10,00, é 264,00" , () => {
+        expect(calcularValorTotalProjeto(listaBasica,10)).toBe(264);
+    })
+    
+    test("Valor do projeto com pacote intemediario e salario 1500" , () => {
+        expect(calcularValorTotalProjeto(listaIntermediaria,calcularValorPorHora(1500))).toBe(645);
+    })
+
     test("Valor do projeto sem funcionalidades e 8 horas seja 0", () => {
         expect(calcularValorTotalProjeto(listaVazia,8)).toBe(0);
     })
-
     
 })
