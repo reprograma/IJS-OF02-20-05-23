83b7099a0e84313d60a8bee141df5ea76c7910c5
andraderika <oliandraderika@gmail.com>
2023-05-20 17:22:41 -0300

    início dos testes e jest funcionando

--- src/entregasDesafio/erika-oliveira/valorHora.test.js
+++ src/entregasDesafio/erika-oliveira/valorHora.test.js
@@ -1,5 +1,12 @@
 const calcularValorPorHora = require('../../dominio/calculadora/Hora/valorHora');
 
 describe("Calcular valor por Hora", () => {
-  test("Salário mínimo")
+  test("Salário mínimo R$ 1.300,00" ,() => {
+
+    expect(calcularValorPorHora(1.300)).toBe(8)
+  });
+
+  test("" , () => {
+
+  })
 })
