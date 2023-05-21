7070698cbaf04b28e2272396d8e92488ca3837b4
Erika Oliveira <109312804+andraderika@users.noreply.github.com>
2023-05-21 15:33:07 -0300

    Update sum.test.js

--- src/entregasDesafio/erika-oliveira/sum.test.js
+++ src/entregasDesafio/erika-oliveira/sum.test.js
@@ -1,5 +1,13 @@
 const sum = require('./sum');
 
-text('adicionando 1 + 2 e tem que retornar 3', () => {
-  expect(sum(1,2)).toBe(3)
-});
+describe("testing the sum function", () => {
+    test("add 1 + 2 and it has to return 3", () => {
+        expect(sum(1, 2)).toBe(3);
+    });
+})
+
+describe("testing broken numbers", () => {
+    test("0.5 + 2.5 = 3", () => {
+        expect(sum(0.5, 2.5)).toBe(3)
+    })
+})
