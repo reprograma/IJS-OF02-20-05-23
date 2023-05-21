385f3e9006ce7cd16682dbb4faa78afa46c13ad5
nathuchoa <nathaliauchoa@hotmail.com>
2023-05-21 14:04:23 +0100

    Criando o teste para o pacote básico

---
+++ src/entregasDesafio/nathalia-uchoa/valorProjeto.test.js
@@ -0,0 +1,24 @@
+const { calcularValorTotalProjeto } = require("../../dominio/calculadora/Projeto/valorProjeto");
+const pacote = require("../../dominio/calculadora/Projeto/pacote.js");
+
+jest.mock("../../dominio/calculadora/Projeto/pacote.js");
+
+describe("Testando o valor do projeto de acordo com o pacote", () => {
+    const valorHora = 8;
+    const funcionalidades = [
+                "setup", 
+                "formulario", 
+                "ssr",
+            ];
+
+    describe("Testando o pacote BÁSICO", () => {
+        beforeEach(() => {
+            pacote.calcularPacote.mockReturnValue("pacote_basico");
+        });
+        test("Retorna o valor total do projeto", () => {
+            const result = calcularValorTotalProjeto(funcionalidades, valorHora);
+            expect(result).toEqual(282);
+        });
+    });
+
+});
