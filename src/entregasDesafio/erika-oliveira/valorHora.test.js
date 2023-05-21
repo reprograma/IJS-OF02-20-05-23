8e22ec6c380209eeaf69670b88c221f2547b14d1
Erika Oliveira <109312804+andraderika@users.noreply.github.com>
2023-05-21 15:44:42 -0300

    Update valorHora.test.js

--- src/entregasDesafio/erika-oliveira/valorHora.test.js
+++ src/entregasDesafio/erika-oliveira/valorHora.test.js
@@ -1,5 +1,15 @@
 const calcularValorPorHora = require('../../dominio/calculadora/Hora/valorHora');
 
-describe("Calcular valor por Hora", () => {
-  test("Salário mínimo")
-})
+describe('Calcular Valor por Hora', () =>{
+  test('Salário mínimo de 1300 reais', () => {
+      const valorPorHoraCalculado = calcularValorPorHora(1300)
+      const valorPorHoraEsperado = 8
+      expect(valorPorHoraCalculado).toBe(valorPorHoraEsperado)
+  });
+
+  test('Valor por hora não pode ser menor que zero', () => {
+      const zero = 0
+      const valorPorHoraCalculado = calcularValorPorHora(1300)
+      expect(valorPorHoraCalculado).toBeGreaterThan(zero)
+  });
+});
