e4e9703bab79389cc152867ca7e192c39bd43547
Jessica Rafaeli Baron <jessica.baron@penseavanti.com.br>
2023-05-21 11:38:06 -0300

    Entrega Jéssica Baron - valorProjeto

--- src/entregasDesafio/jessica-baron/valorProjeto.test.js
+++ src/entregasDesafio/jessica-baron/valorProjeto.test.js
@@ -1,29 +1,31 @@
 const { calcularValorTotalProjeto } = require("../../dominio/calculadora/Projeto/valorProjeto")
+const { calcu } = require("../../dominio/calculadora/Projeto/pacote")
 
-describe ("Cálculo do valor total de um projeto", () => {
+describe("Cálculo do valor total de um projeto", () => {
 
 
     test("Cálculo total de horas do projeto I", () => {
-        
+
         const funcionalidades = [
-         "setup",
-        "formulario",
-        "responsividade",
-        "otimizacao_seo"];
+            "setup",
+            "formulario",
+            "responsividade",
+            "otimizacao_seo"];
         const valorHora = 8;
         const pacote_premium = 200
-     
+
         expect(calcularValorTotalProjeto(funcionalidades, valorHora)).toBeGreaterThan(pacote_premium)
     })
-    test("A taxa contratal desse projeto é de 10%", () => {
-        
+
+    test("A taxa contratual desse projeto é de 10%", () => {
+
         const funcionalidades = [
-         "setup",
-        "otimizacao_seo"];
+            "setup",
+            "otimizacao_seo"
+        ];
         const valorHora = 8;
-        pacote_basico = 400
-     
-        expect(calcularValorTotalProjeto(funcionalidades, valorHora)).toBeLessThanOrEqual(pacote_basico)
+        ceilPacoteBasico = 440
+        expect(calcularValorTotalProjeto(funcionalidades, valorHora)).toBeLessThanOrEqual(ceilPacoteBasico)
     })
 
 })
