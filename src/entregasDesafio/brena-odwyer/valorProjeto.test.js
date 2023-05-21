13918bf10f572889852306a7a0bb45fc4c2d8ef4
brena-odwyer <odwyer.brena@gmail.com>
2023-05-21 09:29:27 -0400

    melhorias teste valorProjeto (teste final ainda não funcionando

--- src/entregasDesafio/brena-odwyer/valorProjeto.test.js
+++ src/entregasDesafio/brena-odwyer/valorProjeto.test.js
@@ -1,17 +1,18 @@
 //Testes para as condições de contorno (quando é básico, intermediário e premium), precisa ter esses 3 testes
 const { calcularValorTotalProjeto } = require('../../dominio/calculadora/Projeto/valorProjeto')
+const { calcularValorPorHora } = require('../../dominio/calculadora/Hora/valorHora')
 
 describe('Calcula o valor total do projeto', () => {
+    const salarioMinimo = 1300
+    const valorHora = calcularValorPorHora(salarioMinimo)
     test('Calcula o valor total do projeto com as taxas contratuais por pacote, sendo um pacote básico', () => {
         const funcionalidadeUnica = ["setup"]
-        const valorHora = 20
-        expect(calcularValorTotalProjeto(funcionalidadeUnica, valorHora)).toBe(176) // valor da hora 20 * 8(setup) * 1.1% do pacote_basico
+        expect(calcularValorTotalProjeto(funcionalidadeUnica, valorHora)).toBe(70) // valor da hora utilizando a função recebendo o salário mínimo * 8(setup) * 1.1% do pacote_basico
 
     })
     test('Calcula o valor total do projeto com as taxas contratuais por pacote, sendo um pacote intermediário', () => {
-        const valorHora = 20
         const funcionalidades = ["setup", "formulario", "responsividade", "otimizacao_seo", "construcao_1_pagina", "integracao_mailchimp", "ssr", "integracao_api_propria"]
-        expect(calcularValorTotalProjeto(funcionalidades, valorHora)).toBe(2392) // valor da hora 20 * 104(soma das funcionalidades) * 1.12% do pacote_basico
+        expect(calcularValorTotalProjeto(funcionalidades, valorHora)).toBe(957) // valor da hora 20 * 104(soma das funcionalidades) * 1.12% do pacote_basico
 
     })
     test('Calcula o valor total do projeto com as taxas contratuais por pacote, sendo um pacote premium', () => {
